87a863bf7c8ff6632a3488ab2f129861a18ea917
chenhuahuiHz <huahui_c@hotmail.com>
2015-09-18 17:16:19 +0800

    down load pics of url

---
+++ pspider.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import re
+import urllib
+from optparse import OptionParser
+
+def get_html(url):
+    try:
+        page = urllib.urlopen(url)
+        html = page.read()
+        return html
+    except IOError as error:
+        print "get html failed: %s" % str(error)
+    return ""
+
+
+def get_img(html):
+    # print html
+    img_reg = r'http[^ ]+\.jpg'
+    p_img_reg = re.compile(img_reg)
+    url_list = re.findall(p_img_reg, html)
+    print url_list
+    index = 0
+    for one_url in url_list:
+        index += 1
+        dst_name = "imag%d.jpg" % index
+        print "downloading %s to %s" % (one_url, dst_name)
+        try:
+            urllib.urlretrieve(one_url, dst_name)
+        except Exception as err:
+            print "error: %s" % str(err)
+
+
+
+ut = "http://www.duba.com/?f=liebaontc"
+
+
+if __name__ == "__main__":
+    parser = OptionParser(usage="usage: %prog [options] arg1")
+    parser.add_option("-u", "--url", dest="url", help="url addr", default="")
+    # parser.add_option("-o", "--once", action="store_true", dest="once", default=False, help="do sync once, then exit")
+    (options, sys.argv) = parser.parse_args()
+
+    if len(options.url) < 1:
+        sys.exit(0)
+    else:
+        get_img(get_html(options.url))
+
